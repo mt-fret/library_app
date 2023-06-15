e050cf197fc6ba874689e71eee955f686c6fc138
Mateusz <mt.fret@gmail.com>
2023-06-15 21:57:06 +0200

    connection to googleAPI established

--- library_app/library/views.py
+++ library_app/library/views.py
@@ -1,10 +1,21 @@
+import json
+
 from django.shortcuts import render
 from django.views.generic import View
-
-
+import requests
+from .keys import google_books_api
 class AddBookView(View):
     def get(self, request):
-        pass
+        return render(request, 'library/find_book.html')
+
+    def post(self, request):
+        isbn = request.POST.get('isbn')
+        books = requests.get(f'https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={google_books_api}')
+        books = books.json()
+        ctx = {
+            'books': books
+        }
+        return render(request, 'library/find_book.html', ctx)
 
 
 class ShelvesView(View):
