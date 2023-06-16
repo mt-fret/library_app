2de66a78888adf96253d9bbb7def06bd532722d4
Mateusz <mt.fret@gmail.com>
2023-06-16 21:33:53 +0200

    getting title out of respond

--- library_app/library/views.py
+++ library_app/library/views.py
@@ -9,12 +9,45 @@ class AddBookView(View):
         return render(request, 'library/find_book.html')
 
     def post(self, request):
-        isbn = request.POST.get('isbn')
-        books = requests.get(f'https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={google_books_api}')
-        books = books.json()
-        ctx = {
-            'books': books
-        }
+        if 'isbn_search' in request.POST:
+            isbn = request.POST.get('isbn')
+            books = requests.get(f'https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={google_books_api}')
+
+            books = books.json()
+
+            books_to_display = []
+
+            for book in books['items']:
+                book = book['volumeInfo']
+                books_to_display.append({'title': book['title'],
+                                         'subtitle': book['subtitle'],
+                                         'autors': book['authors'],
+                                         'published_date': book['publishedDate'],
+                                         'language': book['language']
+                                         })
+            ctx = {
+                'books': books_to_display
+            }
+
+        if 'title_search' in request.POST:
+            title = request.POST.get('title')
+            books = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={title}&key={google_books_api}')
+            books = books.json()
+
+            books_to_display = []
+
+            for book in books['items']:
+                book = book['volumeInfo']
+                books_to_display.append({'title': book['title'],
+                                         'subtitle': book['subtitle'],
+                                         'autors': book['authors'],
+                                         'published_date': book['publishedDate'],
+                                         'language': book['language']
+                                         })
+            ctx = {
+                'books': books_to_display
+            }
+
         return render(request, 'library/find_book.html', ctx)
 
 
